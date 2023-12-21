eacff2fbcc4818b444e910e95de9597a7f177cf3
Vu Dang <vu.dang@menlosecurity.com>
2023-12-21 18:22:36 +0700

    edit

--- flip/js/config.js
+++ flip/js/config.js
@@ -1,32 +1,38 @@
 const config = {
     animation: true,
-    show_raw_data: true,
+    show_raw_data: false,
     show_archived: true,
     remove_team_in_rounds: [2, 1],
+    action_btn: {
+        start: "Start",
+        add: "New",
+        accept: "Accept",
+        reject: "Reject"
+    },
     stage_init: {
         "1": {
-            name: "Round KK",
-            round: 4,
-            prizes_num: 3,
-            btn_text: "GO 4"
-        },
-        "2": {
-            name: "Round 3",
-            round: 3,
-            prizes_num: 3,
-            btn_text: "GO 3"
-        },
-        "3": {
-            name: "Round 2",
-            round: 2,
-            prizes_num: 2,
-            btn_text: "GO 2"
-        },
-        "4": {
             name: "Round 1",
             round: 1,
-            prizes_num: 1,
-            btn_text: "GO 1"
+            max: 1,
+            checked: false,
+        },
+        "2": {
+            name: "Round 2",
+            round: 2,
+            max: 1,
+            checked: false,
+        },
+        "3": {
+            name: "Round 3",
+            round: 3,
+            max: 1,
+            checked: false,
+        },
+        "4": {
+            name: "Round KK",
+            round: 4,
+            max: 1,
+            checked: true,
         }
     }
 };
--- flip/js/script.js
+++ flip/js/script.js
@@ -1,5 +1,6 @@
 const data_file = "data.json";
 let userData;
+let temp_user;
 let archived = {};
 var current_round = 0;
 var conf = config;
@@ -11,12 +12,12 @@ var handleTickInit = function (tick) {
     function (passed) {
       tick.value = val;
     },
-    1500,
+    2000,
     { autostart: false }
   );
   timers[val] = timer;
-  console.log(timers)
-//   timer.start();
+  console.log(timers);
+  //   timer.start();
 };
 
 var loadChampionList = function (data) {
@@ -32,15 +33,21 @@ var initStage = function () {
   var child_tab = document.createElement("div");
   ip_items = "";
   tab_items = "";
+  if (conf.show_raw_data) {
+    show_table = "";
+  } else {
+    show_table = "none";
+  }
   for (var key in data) {
-    const val = data[key].prizes_num;
+    const check = data[key].checked === true ? " checked" : "";
+    const d = data[key].checked === true ? "none" : "none";
     const round = data[key].round;
-    ip_items += `<input type="radio" id="tab-${round}" data-round="${round}" name="tabGroup1" class="tab" onclick="setRoundStage(this)">
-        <label for="tab-${round}">${data[key].name}</label>`;
+    ip_items += `<input type="radio" id="tab-${round}" data-round="${round}" name="tabGroup1" class="tab" onclick="setRoundStage(this)"${check}>
+        <label class="tab-label" id="tab-label-${round}" for="tab-${round}" style="display:${d};">${data[key].name}</label>`;
     tab_items += `<div class="tab__content" id="tabc-${round}">
-          <h3>${data[key].name}</h3>
+          <h1>${data[key].name}</h1>
           <div id="ticker-${round}"></div>
-          <table id="randomData-${round}"><tr><th>ID</th><th>Team</th><th>Prio</th><th>Name</th><th>Status</th></tr></table>
+          <table id="randomData-${round}" style="display:${show_table};"><tr><th>ID</th><th>Team</th><th>Prio</th><th>Name</th><th>Status</th></tr></table>
         </div>`;
   }
   child_ip.innerHTML = ip_items;
@@ -51,20 +58,30 @@ var initStage = function () {
   while (child_tab.firstChild) {
     parent.appendChild(child_tab.firstChild);
   }
+  const r = parent.querySelectorAll('input[type="radio"]');
+  r.forEach((radio) => {
+    if (radio.checked) {
+      setRoundStage(radio);
+    }
+  });
 };
 
 var getData = function (data_file) {
-  fetch(data_file)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      userData = data;
-      //   console.log("userData: ", Object.keys(userData).length, userData);
-    })
-    .catch(function (err) {
-      console.log("error: " + err);
-    });
+  return new Promise((resolve, reject) => {
+    fetch(data_file)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        userData = data;
+        resolve(userData);
+        //   console.log("userData: ", Object.keys(userData).length, userData);
+      })
+      .catch(function (err) {
+        console.log("error: " + err);
+        reject(err);
+      });
+  });
 };
 
 var randomUser = function (data) {
@@ -136,25 +153,64 @@ var addChampionBalls = function (r, data) {
 };
 
 var initRound = function (round, data) {
+  console.log(round);
+  console.log(data);
   var ele_tk = document.getElementById(`ticker-${round}`);
-  var ele_table = document.getElementById(`randomData-${round}`);
-  ele_tk.innerHTML = '';
-  ele_table.style.display = "none";
-  var timer = (5 - round) * 1000;
+  ele_tk.innerHTML = "";
+  //   var timer = (5 - round) * 1000;
+  var timer_1 = (3 - round) * 1000;
+  var timer_2 = (4 - round) * 1000;
   var rduser = randomUser(data);
+  temp_user = rduser;
   console.log("randomUser: ", rduser);
   if (conf.animation) {
     var n_elem = document.createElement("div");
-    var tickerc = `<div id="t-${round}" class="tick" data-value="000" data-did-init="handleTickInit" data-change="${rduser[0]}">
-            <div data-repeat="true" data-layout="horizontal fit" data-transform="arrive(9, .00001) -> round -> pad(\'000\') -> split -> delay(rtl,${timer},${timer})">
+    var tickerc = `<div id="t-${round}" class="tick col-12" data-value="000" data-did-init="handleTickInit" data-change="${rduser[0]}">
+            <div data-repeat="true" data-layout="horizontal fit" data-transform="arrive(9, .00001) -> round -> pad(\'000\') -> split -> delay(rtl,${timer_1},${timer_2})">
             <span data-view="flip"></span></div></div>
-            <div><button type="button" class="btn btn-success btn-lg btn-block btn-roll" id="rollBtn-${round}-${rduser[0]}" data-id="${rduser[0]}" onclick="startRoller(this)">Roll</button>
-            <button type="button" class="btn btn-primary btn-lg btn-block btn-add" id="addBtn-${round}-${rduser[0]}" data-id="${rduser[0]}" onclick="addRoller(this)" style="display:none;"> + </button>
-            <button type="button" class="btn btn-danger btn-lg btn-block btn-remove" id="removeBtn-${round}-${rduser[0]}" data-id="${rduser[0]}" onclick="removeRoller(this)" style="display:none;"> X </button></div>`;
+            <div class="col-12">
+            <button type="button" class="btn btn-outline-success btn-lg btn-block btn-roll col-12" id="rollBtn-${round}-${rduser[0]}" data-id="${rduser[0]}" onclick="startRoller(this)">${conf.action_btn.start}</button>
+            <button type="button" class="btn btn-outline-warning btn-lg btn-block btn-add col-6" id="addBtn-${round}-${rduser[0]}" data-id="${rduser[0]}" onclick="addRoller(this)" style="display:none;">${conf.action_btn.add}</button>
+            <button type="button" class="btn btn-outline-success btn-lg btn-block btn-ok col-6" id="okBtn-${round}-${rduser[0]}" data-id="${rduser[0]}" data-round="${round}" onclick="okRoller(this)" style="display:none;">${conf.action_btn.accept}</button></div>`;
     n_elem.innerHTML = tickerc;
     ele_tk.appendChild(Tick.DOM.create(n_elem, { value: 0 }).root);
     Tick.DOM.parse(n_elem);
   }
+  removeUser(rduser[0]);
+};
+
+var isMaxPrize = function (round) {
+  max_prize = conf.stage_init[round].max;
+  if (archived[round].length >= max_prize) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var startRoller = function (data) {
+  current_roll = data.getAttribute("data-id");
+  timers[current_roll].start();
+  var btnAdd = document.getElementById(
+    `addBtn-${current_round}-${current_roll}`
+  );
+  var btnRemove = document.getElementById(
+    `removeBtn-${current_round}-${current_roll}`
+  );
+  var btnOk = document.getElementById(`okBtn-${current_round}-${current_roll}`);
+  console.log(data, btnAdd, btnRemove);
+  data.style.display = "none";
+  btnAdd.style.display = "";
+  btnOk.style.display = "";
+  playFireWorks(true);
+};
+
+var okRoller = function (data) {
+  current_roll = data.getAttribute("data-id");
+  round = data.getAttribute("data-round");
+  rduser = temp_user;
+  console.log(rduser);
+  var ele_table = document.getElementById(`randomData-${round}`);
   if (conf.show_raw_data) {
     const new_elem = document.createElement("tr");
     new_elem.innerHTML = `<td>${rduser[0]}</td><td>${rduser[1]["team"]}</td>
@@ -166,56 +222,83 @@ var initRound = function (round, data) {
     addChampionList(round);
     addChampionBalls(round, rduser[0]);
   }
-  removeUser(rduser[0]);
   if (conf.remove_team_in_rounds.indexOf(round) !== -1) {
     data = filterDataByGrp(data, "team", rduser[1]["team"]);
   }
   archived[round].push(rduser[0]);
+  data.disabled = true;
   console.log(archived);
-  ele_table.style.display = "";
+  document.getElementById(`addBtn-${round}-${rduser[0]}`).click();
 };
-
-var startRoller = function (data) {
-    current_roll = data.getAttribute("data-id");
-    timers[current_roll].start();
-    var btnAdd = document.getElementById(`addBtn-${current_round}-${current_roll}`);
-    var btnRemove = document.getElementById(`removeBtn-${current_round}-${current_roll}`);
-    console.log(data, btnAdd, btnRemove)
-    data.style.display = 'none';
-    btnAdd.style.display = '';
-    btnRemove.style.display = '';
-}
 
 var addRoller = function (data) {
-    current_roll = data.getAttribute("data-id");
+  current_roll = data.getAttribute("data-id");
+  if (isMaxPrize(current_round) === false) {
     initRound(current_round, userData);
-}
-
-var removeRoller = function (data) {
-    current_roll = data.getAttribute("data-id");
-    timers[current_roll].start();
-}
-
-var setRoundStage = function (tab) {
-  current_round = tab.getAttribute("data-round");
-  archived[current_round] = [];
-  initRound(current_round, userData);
-  //   initButtons();
+  } else {
+    showNotification(
+      `Reached the Maximum ${conf.stage_init[current_round].max} prizes of round ${current_round}`
+    );
+    var next_round = round - 1;
+    if ( next_round > 0) {
+        document.getElementById(`tab-${next_round}`).click();
+    } else {
+        endGame();
+    }
+  }
+  data.disabled = true;
 };
 
-var initButtons = function () {
-  const buttonr = document.getElementById(`randomBtnR${current_round}`);
-  buttonr.addEventListener("click", function () {
-    callRound("R", current_round, userData);
-    userData = filterDataByGrp(userData, "priority", current_round);
-    buttonr.style.display = "none";
-  });
+var setRoundStage = function (data) {
+  current_round = data.getAttribute("data-round");
+  archived[current_round] = [];
+  initRound(current_round, userData);
+};
+
+var playFireWorks = function (data) {
+  const page = "#main-page";
+  const container = $(page);
+  if (data === true) {
+    container.fireworks();
+  } else {
+    container.fireworks("destroy");
+  }
+};
+
+var endGame = function (params) {
+    playFireWorks(false);
+    document.getElementById('main-page').classList.add('fade-out');
+    const cl = document.getElementById('champions-list');
+    setTimeout(function(){
+        document.getElementById('main-page').style.display = "none";
+        cl.classList.remove('col-4');
+        cl.classList.add('col-12');
+    },3000);
+}
+
+var showNotification = function (data) {
+  const notification = document.querySelector(".notification-container");
+  notification.querySelector('.notification').textContent = data;
+  notification.style.display = "block"; // Set the display property to 'block'
+  setTimeout(hideNotification, 5000);
+};
+
+// Function to hide the notification
+var hideNotification = function (data) {
+  const notification = document.querySelector(".notification-container");
+  notification.style.display = "none"; // Set the display property to 'none'
 };
 
 var init = function () {
-  loadChampionList("champions-list");
-  getData(data_file);
-  initStage();
+  loadChampionList("page-container");
+  getData(data_file)
+    .then(() => {
+      initStage();
+    })
+    .catch((error) => {
+      // Handle errors if getData() fails
+      console.error("Error while getting data:", error);
+    });
 };
 
 window.onload = function () {
